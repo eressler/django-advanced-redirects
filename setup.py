2ae6a2a8ab2200efe3039e7c74e3736188d74a25
Eric Ressler <ericr@smashingideas.com>
2014-12-04 15:57:23 -0800

    updated setup again

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ VERSION = __import__("advanced_redirects").__version__
 
 setup(
     name='django-advanced-redirects',
-    packages=['django-advanced-redirects'],  # this must be the same as the name above
+    packages=['advanced_redirects'],
     version=VERSION,
     description='Advanced redirect management for Django applications.',
     author='Eric Ressler',
     author_email='eric@ericressler.com',
-    url='http://eressler.github.io/django-advanced-redirects/',  # use the URL to the github repo
-    download_url="https://github.com/eressler/django-advanced-redirects/tarball/{0}".format(VERSION),  # I'll explain this in a second
-    keywords=['django', 'redirects', 'seo'], # arbitrary keywords
+    url='http://eressler.github.io/django-advanced-redirects/',
+    download_url="https://github.com/eressler/django-advanced-redirects/tarball/{0}".format(VERSION),
+    keywords=['django', 'redirects', 'seo'],
     classifiers=[],
 )
